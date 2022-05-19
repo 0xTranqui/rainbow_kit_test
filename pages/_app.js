54b5dce63068559cfd393f727a129fcfe36aaf93
0xTranqui <thinkandchill11@gmail.com>
2022-05-19 00:56:19 -0300

    basic set up with rainbow kit connect button

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
 import '../styles/globals.css'
 
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  apiProvider,
+  configureChains,
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme, midnightTheme
+} from '@rainbow-me/rainbowkit';
+import { chain, createClient, WagmiProvider } from 'wagmi';
+
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.rinkeby],
+  [
+    apiProvider.alchemy(process.env.ALCHEMY_ID),
+    apiProvider.fallback()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiProvider client={wagmiClient}>
+      <RainbowKitProvider  chains={chains} theme={midnightTheme()}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+    </WagmiProvider>    
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,15 +14,18 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+{/*         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
-        </p>
+        </p> */}
 
+        <ConnectButton label="New ish" />
+
+{/* 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
@@ -49,7 +54,7 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
       </main>
 
       <footer className={styles.footer}>
